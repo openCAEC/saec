9e9b0065b45df844906a10990c7ce4646486a63c
Gustavo Simões <gustavosimoesmendonca@gmail.com>
2021-09-10 00:05:00 -0300

    responsive bug fix
    Signed-off-by: Gustavo Simões <gustavosimoesmendonca@gmail.com>

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 exports.components = {
   "component---cache-dev-404-page-js": preferDefault(require("/Users/gustavo/Documents/dev/code/ufsc/public_html/.cache/dev-404-page.js")),
-  "component---src-pages-404-tsx": preferDefault(require("/Users/gustavo/Documents/dev/code/ufsc/public_html/src/pages/404.tsx")),
+  "component---src-pages-404-js": preferDefault(require("/Users/gustavo/Documents/dev/code/ufsc/public_html/src/pages/404.js")),
   "component---src-pages-index-tsx": preferDefault(require("/Users/gustavo/Documents/dev/code/ufsc/public_html/src/pages/index.tsx"))
 }
 
--- public/render-page.js
+++ public/render-page.js
@@ -14,6 +14,7 @@
 
 exports.ssrComponents = {
   "component---cache-dev-404-page-js": preferDefault(__webpack_require__(/*! ./.cache/dev-404-page.js */ "./.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(__webpack_require__(/*! ./src/pages/404.js */ "./src/pages/404.js")),
   "component---src-pages-index-tsx": preferDefault(__webpack_require__(/*! ./src/pages/index.tsx */ "./src/pages/index.tsx"))
   }
 
@@ -5867,6 +5868,41 @@ exports.onRenderBody = function (_ref, pluginOptions) {
     href: withPrefix(_path.posix.join(output, "/sitemap-index.xml"))
   })]);
 };
+
+/***/ }),
+
+/***/ "./src/pages/404.js":
+/*!**************************!*\
+  !*** ./src/pages/404.js ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _home_module_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./home.module.scss */ "./src/pages/home.module.scss");
+
+
+
+const IndexPage = () => {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(react__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h3", {
+    className: _home_module_scss__WEBPACK_IMPORTED_MODULE_1__.tag
+  }, "Em breve!"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("main", {
+    className: _home_module_scss__WEBPACK_IMPORTED_MODULE_1__.main
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("title", null, "SAEC 2021 | Em contru\xE7\xE3o"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("img", {
+    className: _home_module_scss__WEBPACK_IMPORTED_MODULE_1__.logo,
+    alt: "Logo SAEC",
+    src: "/logo.svg"
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: _home_module_scss__WEBPACK_IMPORTED_MODULE_1__.socialMedia
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h3", null, "Siga-nos nas redes sociais"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1", null, "/saecufscara"))));
+};
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (IndexPage);
 
 /***/ }),
 
