19ae32404715b9566ed7ebdb4ec409baecad2dc4
Gustavo Simões <gustavosimoesmendonca@gmail.com>
2021-10-08 19:12:50 -0300

    Material UI adicionado
    Drawer adicionado
    
    Signed-off-by: Gustavo Simões <gustavosimoesmendonca@gmail.com>

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,32 +1,50 @@
 import { Link } from "gatsby";
 import * as React from "react";
+import Drawer from "@material-ui/core/Drawer";
 
 import * as styles from "./header.module.scss";
 import { links } from "./links";
 
 const HeaderComponent = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
+  function getLinks() {
+    return links.map((link) => {
+      if (!link.disableCase || !link.disableCase.includes("desktop")) {
+        return (
+          <Link
+            key={link.id}
+            to={link.path}
+            className={
+              link.className
+                ? `${styles.link} ${styles[link.className]}`
+                : styles.link
+            }
+          >
+            {link.name}
+          </Link>
+        );
+      }
+    });
+  }
+
   return (
     <header className={styles.header}>
-      <nav>
-        {links.map((link) => {
-          if (!link.disableCase || !link.disableCase.includes("desktop")) {
-            return (
-              <Link
-                key={link.id}
-                to={link.path}
-                className={
-                  link.className
-                    ? `${styles.link} ${styles[link.className]}`
-                    : styles.link
-                }
-              >
-                {link.name}
-              </Link>
-            );
-          }
-        })}
-        ;
-      </nav>
+      <button onClick={() => toggleDrawer()}>drawer</button>
+
+      <Drawer
+        anchor={"left"}
+        open={isDrawerOpen}
+        onClose={() => toggleDrawer()}
+      >
+        {getLinks()}
+      </Drawer>
+
+      <nav>{getLinks()}</nav>
     </header>
   );
 };
