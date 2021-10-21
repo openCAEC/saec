6be533f8d0e4272de37d688dc437dc9f59353a3a
Gustavo Simões <gustavosimoesmendonca@gmail.com>
2021-10-21 00:35:38 -0300

    Componente com as mídias socias da SAEC criado
    Signed-off-by: Gustavo Simões <gustavosimoesmendonca@gmail.com>

---
+++ src/components/socialMedia/socialMediaResources.ts
@@ -0,0 +1,53 @@
+type Social = {
+  name: string;
+  iconPath: string;
+  linkTo: string;
+};
+
+export const socialMediaResources: Array<Social> = [
+  {
+    name: "Discord",
+    iconPath: "/social/discord.svg",
+    linkTo: "https://saec.ufsc.br/discord",
+  },
+  {
+    name: "Facebook",
+    iconPath: "/social/facebook.svg",
+    linkTo: "https://facebook.com/saecufscara",
+  },
+  {
+    name: "GitHub",
+    iconPath: "/social/github.svg",
+    linkTo: "https://github.com/openCAEC/saec",
+  },
+  {
+    name: "Instagram",
+    iconPath: "/social/instagram.svg",
+    linkTo: "https://instagram.com/saecufscara",
+  },
+  {
+    name: "Linkedin",
+    iconPath: "/social/linkedin.svg",
+    linkTo: "https://www.linkedin.com/company/caecufsc/",
+  },
+  {
+    name: "Spotify",
+    iconPath: "/social/spotify.svg",
+    linkTo: "SEM",
+  },
+  {
+    name: "TikTok",
+    iconPath: "/social/tiktok.svg",
+    linkTo: "https://tiktok.com/saecufscara",
+  },
+  {
+    name: "Twitter",
+    iconPath: "/social/twitter.svg",
+    linkTo: "https://twitter.com/saecufscara",
+  },
+  {
+    name: "Youtube",
+    iconPath: "/social/youtube.svg",
+    linkTo: "https://youtube.com/saecufscara",
+  },
+];
