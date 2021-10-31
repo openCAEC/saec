ed65a3e3b8ff39ce0b46e56ee2ef1628db32aa3c
Gustavo Simões <gustavosimoesmendonca@gmail.com>
2021-10-31 20:12:17 -0300

    Tente outra vez - Raul Seixas
    Voltado projeto pro começo
    
    Signed-off-by: Gustavo Simões <gustavosimoesmendonca@gmail.com>

--- gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-import { AuthUserProvider } from "./src/contexts/authUser";
-import "./src/styles/global.scss";
-
-export const wrapRootElement = ({ element }) => (
-  <AuthUserProvider>{element}</AuthUserProvider>
-);
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,48 +1,22 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "saec",
+    title: "SAEC | 2021",
   },
   plugins: [
-    {
-      resolve: `gatsby-plugin-typescript`,
-      options: {
-        isTSX: true, // defaults to false
-        jsxPragma: `jsx`, // defaults to "React"
-        allExtensions: true, // defaults to false
-      },
-    },
     "gatsby-plugin-sass",
-    "gatsby-plugin-image",
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: "TRACKING_ID_GOOGLE_ANALYTCS",
+        trackingId: "TRACKING_ID",
       },
     },
-    "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        name: `SAEC UFSC`,
-        short_name: `SAEC`,
-        start_url: `/`,
-        background_color: `#3f00f2`,
-        theme_color: `#3f00f2`,
-        display: `standalone`,
         icon: "src/images/icon.png",
       },
-    },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
     },
   ],
 };
